fa8c2bb1d45edc119ad919ccf336f4cfe57afce2
victor-amoros <victor.amoros96@gmail.com>
2025-06-26 14:39:51 +0200

    created run_python.py

--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
-from functions.write_file import write_file
+from functions.run_python import run_python_file
 
-
-print(write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"))
-print(write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
-print(write_file("calculator", "/tmp/temp.txt", "this should not be allowed"))
+print(run_python_file("calculator", "main.py"))
+print(run_python_file("calculator", "tests.py"))
+print(run_python_file("calculator", "../main.py"))
+print(run_python_file("calculator", "nonexistent.py"))
